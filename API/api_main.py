cb4415085823792b0712c1c0eaf3d7152b12fe99
Ohm_2018 <102822222+DVTZOE@users.noreply.github.com>
2023-06-20 13:38:16 +0700

    bot main added (wip)

--- API/api_main.py
+++ API/api_main.py
@@ -5,28 +5,28 @@ import questionary
 
 all_assingments = get_api.get_assingments()
 
-coursechoices = [
-    {
-        'type': 'select',
-        'message': "Please Sign-In before accessing the database",
-        'name': 'login',
-        "choices": all_assingments.keys()
-    }
-]
+# coursechoices = [
+#     {
+#         'type': 'select',
+#         'message': "Please Sign-In before accessing the database",
+#         'name': 'login',
+#         "choices": all_assingments.keys()
+#     }
+# ]
 
-course = questionary.prompt(coursechoices)
+# course = questionary.prompt(coursechoices)
 
-all_assingments.get(course)
+# all_assingments.get(course)
 
-# for course, assingments in all_assingments.items():
-#     print(course)
-#     for assingment in assingments:
-#         if isinstance(assingment.get('description'), str):
-#             if "Import grade" in assingment.get('description'):
-#                 continue
-#         for key, value in assingment.items():
-#             if key == 'description':
-#                 print(f"{key}:\n{value}")
-#             else:
-#                 print(f"\t{key}: {value}")
-#     print('\n')
+for course, assingments in all_assingments.items():
+    print(course)
+    for assingment in assingments:
+        if isinstance(assingment.get('description'), str):
+            if "Import grade" in assingment.get('description'):
+                continue
+        for key, value in assingment.items():
+            if key == 'description':
+                print(f"{key}:\n{value}")
+            else:
+                print(f"\t{key}: {value}")
+    print('\n')
---
+++ bot/bot_main.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands
+import os
+
+secrets_dir = os.path.dir(os.path.abspath(__file__))
+token_path = os.path.join(secrets_dir, 'token.txt')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='', intents=intents)
+
+@bot.command()
+async def test(ctx):
+    pass
+
+client = discord.Client(intents=intents)
+
+client.run('MTEyMDU1MTMxMzIzNTc3OTczNg.GhtISb.R2McLyK3lSI5I5GIElQgUO4UzvK_DrGEfPb_G4')
